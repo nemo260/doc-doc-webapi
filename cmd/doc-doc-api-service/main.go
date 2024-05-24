6e57e8f5218107f270868905d6cfd596dcb5f045
Martin Nemec <nneemmeecc.m@gmail.com>
2024-05-24 11:16:46 +0200

    Kostra servera

--- cmd/doc-doc-api-service/main.go
+++ cmd/doc-doc-api-service/main.go
@@ -1,26 +1,29 @@
 package main
 
 import (
-    "log"
-    "os"
-    "strings"
-    "github.com/gin-gonic/gin"
-    "github.com/nemo260/doc-doc-webapi/api"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/nemo260/doc-doc-webapi/api"
+	doc_doc_webapi "github.com/nemo260/doc-doc-webapi/internal/doc-doc-webapi"
 )
 
 func main() {
-    log.Printf("Server started")
-    port := os.Getenv("AMBULANCE_API_PORT")
-    if port == "" {
-        port = "8080"
-    }
-    environment := os.Getenv("AMBULANCE_API_ENVIRONMENT")
-    if !strings.EqualFold(environment, "production") { // case insensitive comparison
-        gin.SetMode(gin.DebugMode)
-    }
-    engine := gin.New()
-    engine.Use(gin.Recovery())
-    // request routings
-    engine.GET("/openapi", api.HandleOpenApi)
-    engine.Run(":" + port)
+	log.Printf("Server started")
+	port := os.Getenv("AMBULANCE_API_PORT")
+	if port == "" {
+		port = "8080"
+	}
+	environment := os.Getenv("AMBULANCE_API_ENVIRONMENT")
+	if !strings.EqualFold(environment, "production") { // case insensitive comparison
+		gin.SetMode(gin.DebugMode)
+	}
+	engine := gin.New()
+	engine.Use(gin.Recovery())
+	// request routings
+	doc_doc_webapi.AddRoutes(engine)
+	engine.GET("/openapi", api.HandleOpenApi)
+	engine.Run(":" + port)
 }
---
+++ internal/doc-doc-webapi/impl_docs.go
@@ -0,0 +1,27 @@
+package doc_doc_webapi
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (this *implDocsAPI) AddDocument(ctx *gin.Context) {
+	ctx.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func (this *implDocsAPI) DeleteDocument(ctx *gin.Context) {
+	ctx.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func (this *implDocsAPI) GetDocumentById(ctx *gin.Context) {
+	ctx.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func (this *implDocsAPI) GetDocuments(ctx *gin.Context) {
+	ctx.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func (this *implDocsAPI) UpdateDocument(ctx *gin.Context) {
+	ctx.AbortWithStatus(http.StatusNotImplemented)
+}
